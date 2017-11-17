c476983c8f36a0c69763b65b5be1f8126ffcedf9
Iarlen <iarlensilva775@gmail.com>
2017-11-16 21:53:22 -0300

    criando o método getOperacao

--- src/br/ufc/quixada/app/Cliente.java
+++ src/br/ufc/quixada/app/Cliente.java
@@ -3,8 +3,12 @@ package br.ufc.quixada.app;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
+
+import br.ufc.quixada.calc.Calculadora.Request;
 
 public class Cliente {
+	static int id = 0;
 
 	public static void main(String[] args) {
 
@@ -13,9 +17,23 @@ public class Cliente {
 	public static void iniciarCliente(String ip, int porta) throws UnknownHostException, IOException {
 		Socket cliente = new Socket(ip, porta);
 	}
-	
-	public static Request obterDados(){
-		
+
+	public static Request obterDados() {
+		Scanner entrada = new Scanner(System.in);
+		Request.Builder req = Request.newBuilder();
+
+		req.setId(++id);
+
+		System.out.println("Digite o primeiro numero!");
+		req.setN1(entrada.nextDouble());
+
+		System.out.println("Digite a operação!");
+		String op = entrada.nextLine();
+
 		return null;
+	}
+
+	public static int getOperacao(String op) {
+		return 0;
 	}
 }
