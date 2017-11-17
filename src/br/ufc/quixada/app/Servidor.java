503637992155319414c71458b72579011b122993
Iarlen <iarlensilva775@gmail.com>
2017-11-16 21:14:48 -0300

    criando método iniciar servidor

--- src/br/ufc/quixada/app/Servidor.java
+++ src/br/ufc/quixada/app/Servidor.java
@@ -1,13 +1,22 @@
 package br.ufc.quixada.app;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
+
+
 public class Servidor {
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		iniciarServidor(8789);
 	}
 	
-	public void iniciarServidor(int porta){
+	public static void iniciarServidor(int porta) throws IOException{
+		ServerSocket socket = null;
 		
+		socket = new ServerSocket(porta);
+		
+		System.out.println("Servidor rodando");
 	}
 
 }
