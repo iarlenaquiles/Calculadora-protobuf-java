554508697b4bd1341361725ba7134fbe5c33c147
Iarlen <iarlensilva775@gmail.com>
2017-11-16 21:45:10 -0300

    criando o método iniciarCliente

--- src/br/ufc/quixada/app/Cliente.java
+++ src/br/ufc/quixada/app/Cliente.java
@@ -1,12 +1,21 @@
 package br.ufc.quixada.app;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Cliente {
 
 	public static void main(String[] args) {
-		
+
+	}
+
+	public static void iniciarCliente(String ip, int porta) throws UnknownHostException, IOException {
+		Socket cliente = new Socket(ip, porta);
 	}
 	
-	public static void iniciarCliente(){
+	public static Request obterDados(){
 		
+		return null;
 	}
 }
