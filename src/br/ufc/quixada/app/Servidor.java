874f01421a60a895f5e56be423b5469d3bed6ea7
Iarlen <iarlensilva775@gmail.com>
2017-11-16 21:26:38 -0300

    melhorando o método calcular

--- src/br/ufc/quixada/app/Servidor.java
+++ src/br/ufc/quixada/app/Servidor.java
@@ -13,44 +13,52 @@ import br.ufc.quixada.calc.Calculadora.Reply;
 import br.ufc.quixada.calc.Calculadora.Request;
 import br.ufc.quixada.calc.Calculadora.Request.Operacao;
 
-
 public class Servidor {
-	
+
 	public static void main(String[] args) throws IOException {
 		iniciarServidor(8789);
 	}
-	
-	public static void iniciarServidor(int porta) throws IOException{
+
+	public static void iniciarServidor(int porta) throws IOException {
 		ServerSocket socket = new ServerSocket(porta);
-		
+
 		System.out.println("Servidor rodando");
-		
-		while(true){
+
+		while (true) {
 			Socket sock = socket.accept();
-			
+
 			Request req = Request.parseDelimitedFrom(sock.getInputStream());
-			
+
 			Double resultado = calcular(req);
-			
+
 			Reply res = Reply.newBuilder().setId(req.getId()).setRes(resultado).build();
-			
+
 			res.writeDelimitedTo(sock.getOutputStream());
-			
+
 		}
 	}
-	
-	public static double calcular(Request req){
+
+	public static double calcular(Request req) {
 		Operacao op = req.getOp();
 		double n1 = req.getN1();
 		double n2 = req.getN2();
-		
-		if(op == SOM){
-			return n1+n2;
-		}else if(op==SUB){
-			
+
+		if (op == SOM) {
+			return n1 + n2;
+		} else if (op == SUB) {
+			return n1 - n2;
+		} else if (op == MUL) {
+			return n1 * n2;
+		} else if (op == DIV) {
+			if (n2 > 0) {
+				return n1 / n2;
+			} else {
+				return 0;
+			}
+		} else {
+			return 0;
 		}
-		
-		return 0;
+
 	}
 
 }
